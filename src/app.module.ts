58739e52acbf8628cb969e6fa272576a1638a3fc
“shagor7” <mdsagorparvez@gmail.com>
2022-11-09 18:59:18 +0600

    testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { AccountController } from './account/account.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, AccountController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
+import { request } from 'http';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+  // @Get()
+  // findAll(@Req() request: Request): string {
+  //   return 'this action returns all cats';
+  // }
+  @Get('ab*cd')
+  findAl() {
+    return 'This route uses a wildcard';
+  }
+  @Post()
+  @HttpCode(204)
+  createe() {
+    return 'Thsi action adds a new cat';
+  }
   @Get()
-  findAll(): string {
-    return 'this action returns all cats';
+  @Redirect('https://nestjs.com', 301)
+  profile(): string {
+    return 'nothing'
+  }
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version: any) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action return a #${params.id} cat`;
   }
 }
